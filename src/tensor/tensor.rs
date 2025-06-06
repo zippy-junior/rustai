b4089486d90e846ed18e3d44fb48ed319e592e30
AZagoruyko <AZagoruyko@ibs.ru>
2025-06-06 11:54:26 +0300

    Add multiplicatioin and addition with scalar values to tensor

--- src/tensor/tensor.rs
+++ src/tensor/tensor.rs
@@ -46,6 +46,26 @@ where
     }
 }
 
+impl<T, const ROWS: usize, const COLS: usize> ops::Add<T> for Tensor<T, ROWS, COLS>
+where
+    T: ops::Add<Output = T> + Copy,  // Element type must support addition and be copyable
+{
+    type Output = Self;  // Result of addition is another Tensor with same dimensions
+
+    fn add(self, scalar: T) -> Tensor<T, ROWS, COLS> {
+        let mut result = self;  // Copy self (requires T: Copy)
+        
+        // Element-wise addition
+        for i in 0..ROWS {
+            for j in 0..COLS {
+                result.data[i][j] = result.data[i][j] + scalar;
+            }
+        }
+        
+        result
+    }
+}
+
 impl<T, const LH_ROWS: usize, const LH_COLS: usize, const RH_ROWS: usize, const RH_COLS: usize> ops::Mul<Tensor<T, RH_ROWS, RH_COLS>> for Tensor<T, LH_ROWS, LH_COLS>
 where
     T: ops::Add<Output = T> + Default + ops::Mul<Output = T> + Copy + std::fmt::Debug,  // Element type must support addition and be copyable
@@ -70,6 +90,25 @@ where
             }
         }
         
+        result
+    }
+}
+
+impl<T, const ROWS: usize, const COLS: usize> ops::Mul<T> for Tensor<T, ROWS, COLS>
+where
+    T: ops::Add<Output = T> + Default + ops::Mul<Output = T> + Copy,  // Element type must support addition and be copyable
+{
+    type Output = Tensor<T, ROWS, COLS>;  // Result of addition is another Tensor with same dimensions
+
+    fn mul(self, scalar: T) -> Tensor<T, ROWS, COLS> {
+        let mut result = self;
+        
+        for i in 0..ROWS {
+            for j in 0..COLS {
+                result.data[i][j] = result.data[i][j] * scalar;
+            }
+        }
+                
         result
     }
 }
